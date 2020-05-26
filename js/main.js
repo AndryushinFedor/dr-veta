73a31baace48ae1dfcc52a7da4579a92458ff7d1
Fedor <andryushin.fedor@gmail.com>
2020-05-26 11:03:51 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function setup() {
+    let myCanvas = createCanvas(windowWidth, windowHeight);
+    myCanvas.parent("canvas");
+    pixelDensity(1);
+  }
+
+  function draw() {
+    background(51);
+  
+    loadPixels();
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        var index = (x + y * width) * 4;
+        pixels[index + 0] = x;
+        pixels[index + 1] = random(255);
+        pixels[index + 2] = y;
+        pixels[index + 3] = 255;
+      }
+    }
+    updatePixels();
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
